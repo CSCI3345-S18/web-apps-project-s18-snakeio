50f75f9ceb14f109d50c8e6c69a6d5a4e99475f9
2015-08-26 <szbidi@xena09.cs.trinity.edu>
2018-04-04 20:11:44 -0500

    slight website changes

--- game.js
+++ game.js
@@ -1,36 +1,85 @@
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
-var headx = Math.floor(Math.random()*(c.width)+1);
-var heady = Math.floor(Math.random()*(c.height)+1);
-var direction = Math.floor(Math.random()*4)+1;
 
-function draw(){
-console.log(direction);
 ctx.fillStyle = "#262626";
 ctx.rect(0,0,c.width,c.height);
 ctx.fill();
+
 ctx.beginPath();
-ctx.arc(headx,heady,20,0,2*Math.PI);
+ctx.arc(40,40,20,0,2*Math.PI);
 ctx.fillStyle = "red";
 ctx.fill();
-}
+ctx.beginPath();
+ctx.arc(80,40,20,0,2*Math.PI);
+ctx.fillStyle = "red";
+ctx.fill();
+ctx.beginPath();
+ctx.arc(120,40,20,0,2*Math.PI);
+ctx.fillStyle = "red";
+ctx.fill();
+ctx.beginPath();
+ctx.arc(160,40,20,0,2*Math.PI);
+ctx.fillStyle = "red";
+ctx.fill();
+ctx.beginPath();
+ctx.arc(205,40,25,0,2*Math.PI);
+ctx.fillStyle = "red";
+ctx.fill();
 
-function move(){
-if(direction === 1){
-		headx+=1;
-		draw();
 
-}else if (direction === 2){
-		headx-=1;
-		draw();
-}else if (direction === 3){
-		heady+=1;
-		draw();
 
-}else{
-		heady-=1;
-		draw();
-}
-}
+ctx.beginPath();
+ctx.arc(400,100,20,0,2*Math.PI);
+ctx.fillStyle = "yellow";
+ctx.fill();
+ctx.beginPath();
+ctx.arc(400,140,20,0,2*Math.PI);
+ctx.fillStyle = "yellow";
+ctx.fill();
+ctx.beginPath();
+ctx.arc(400,180,20,0,2*Math.PI);
+ctx.fillStyle = "yellow";
+ctx.fill();
+ctx.beginPath();
+ctx.arc(360,180,20,0,2*Math.PI);
+ctx.fillStyle = "yellow";
+ctx.fill();
+ctx.beginPath();
+ctx.arc(315,180,25,0,2*Math.PI);
+ctx.fillStyle = "yellow";
+ctx.fill();
 
-setInterval(move(),10);
+
+
+ctx.beginPath();
+ctx.arc(500,360,20,0,2*Math.PI);
+ctx.fillStyle = "blue";
+ctx.fill();
+ctx.beginPath();
+ctx.arc(460,360,20,0,2*Math.PI);
+ctx.fillStyle = "blue";
+ctx.fill();
+ctx.beginPath();
+ctx.arc(420,360,20,0,2*Math.PI);
+ctx.fillStyle = "blue";
+ctx.fill();
+ctx.beginPath();
+ctx.arc(380,360,20,0,2*Math.PI);
+ctx.fillStyle = "blue";
+ctx.fill();
+ctx.beginPath();
+ctx.arc(335,360,25,0,2*Math.PI);
+ctx.fillStyle = "blue";
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = "limegreen";
+ctx.rect(100,300,25,25);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = "limegreen";
+ctx.rect(525,50,25,25);
+ctx.fill();
+
+
